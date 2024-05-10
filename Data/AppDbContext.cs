f5e20623d2e01f88d32e323737d48729634e5574
AdilKalbaev04 <adilkalbaev2004@gmail.com>
2024-05-10 16:54:14 +0600

    init

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+// Data/AppDbContext.cs
+
+using CSharpCornerApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CSharpCornerApi.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CSharpCornerArticle> Articles { get; set; }
+    }
+}
